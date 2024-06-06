03dd3444227872f49d7409e8bb71119ea0b71e2c
tmeunier <theo.meunier41@gmail.com>
2024-06-06 20:15:29 +0200

    fix: upload folder and file

--- front/src/components/Dropzone/Dropzone.tsx
+++ front/src/components/Dropzone/Dropzone.tsx
@@ -3,10 +3,12 @@ import {ReactNode, useCallback} from "react";
 import './dropzone.css';
 import {useFileStore} from "../../stores/useFileStore.ts";
 import {useAxios} from "../../config/axios.ts";
+import {useQueryClient} from "react-query";
 
 export function Dropzone({children}: { children: ReactNode }) {
     const {setFiles} = useFileStore();
     const API = useAxios()
+    const queryClient = useQueryClient()
 
     const handleFileUpload = async (file: File) => {
         const chunkSize = 1024 * 1024 * 5; // 5 MB chunk size
@@ -14,7 +16,7 @@ export function Dropzone({children}: { children: ReactNode }) {
 
         // initialisation de l'upload
         const initResponse = await API.post("/folders/upload/init", {
-                filename: file.name,
+                filename: file.webkitRelativePath,
                 total_chunks: totalChunks,
             },
             {
@@ -33,7 +35,7 @@ export function Dropzone({children}: { children: ReactNode }) {
             formData.append('uploadId', uploadId.toString());
             formData.append('chunkNumber', (index + 1).toString());
             formData.append('totalChunks', totalChunks.toString());
-            formData.append('file', chunk, file.name);
+            formData.append('file', chunk, file.webkitRelativePath);
 
             // Assurez-vous que `mutate` est asynchrone et retourne une promesse
             await API.post("/folders/upload", formData, {
@@ -46,19 +48,22 @@ export function Dropzone({children}: { children: ReactNode }) {
         // Finalisation de l'upload
         await API.post("/folders/upload/complete", {
             upload_id: uploadId.toString(),
-            filename: file.name,
+            filename: file.webkitRelativePath,
         });
+
+        await queryClient.invalidateQueries("storage")
     };
 
     const onDrop = useCallback((acceptedFiles: File[]) => {
         acceptedFiles.forEach(handleFileUpload);
     }, [setFiles]);
 
-    const {getRootProps, isDragActive} = useDropzone({onDrop});
+    const {getRootProps, isDragActive} = useDropzone({
+        onDrop,
+    });
 
     return (
         <div {...getRootProps()} className={`relative cols-span-3 px-6 h-full`}>
-
             {children}
 
             <div className={`dropzone__file ${isDragActive ? 'active' : ''}`}></div>
