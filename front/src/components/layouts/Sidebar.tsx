fc1499154f75ea35fa1c08dd4007f137f2f843de
tmeunier <theo.meunier41@gmail.com>
2024-08-09 01:03:24 +0200

    fix: sidebar isActive

--- front/src/components/layouts/Sidebar.tsx
+++ front/src/components/layouts/Sidebar.tsx
@@ -45,7 +45,17 @@ export function SidebarMenuItem({svg: SvgComponent, children, href, onClick}: {
     onClick?: (event: MouseEvent) => void | undefined
 }) {
     const location = useLocation();
-    const isActive = location.pathname.startsWith(href!) ? 'border-indigo-700 text-indigo-700 bg-indigo-50' : 'text-gray-600 border-transparent';
+    let isActive;
+
+    console.log(location.pathname)
+
+    if (location.pathname === href) {
+        isActive = 'border-indigo-700 text-indigo-700 bg-indigo-50';
+    } else  if (location.pathname === '/' && href === '/'){
+        isActive = 'text-gray-600 border-transparent';
+    } else {
+        isActive = 'text-gray-600 border-transparent';
+    }
 
     return <>
         <li className={`${isActive}  pr-2 py-2 text-sm hover:text-indigo-700 border-l-4  hover:border-indigo-700 hover:bg-indigo-50`}>
